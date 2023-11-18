427242caa6113a345f1c44caa95efb23d3f1ffb7
yaqinzz <ahmadainulyaqin131@gmail.com>
2023-11-18 11:37:36 +0700

    p

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ import authRouters from './routers/auth.js'
 import adminRouters from './routers/userAdmin.js'
 import customerRoters from './routers/userCustomer.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
 const port = 3000
 
 //middlewares
 app.use(express.json())
-app.use(express.json())
 app.use((req, res, next) => {
+  app.use(express.json())
   res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
-
+app.use(
+  cors({
+    origin: '*',
+  })
+)
 app.use(cookieParser())
 
 app.use('/api/auth', authRouters)
