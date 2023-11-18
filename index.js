1ad64870b6fd8d33b52ed00fa139cd030780ac13
yaqinzz <ahmadainulyaqin131@gmail.com>
2023-11-18 11:00:00 +0700

    cros

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.use(express.json())
 app.use(
   cors({
-    origin: 'https://bengkel-api-nine.vercel.app/',
+    origin: 'https://bengkel-api-zeta.vercel.app',
   })
 )
 app.use(cookieParser())
