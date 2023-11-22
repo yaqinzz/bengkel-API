fccec80a951c7dd1e283a60327eff654134322c3
yaqinzz <ahmadainulyaqin131@gmail.com>
2023-11-22 11:58:54 +0700

    test

--- connect.js
+++ connect.js
@@ -1,9 +1,10 @@
 import {Sequelize} from 'sequelize'
 import dotenv from 'dotenv'
-
+import mysql2 from 'mysql2'
 dotenv.config()
 export const db = new Sequelize({
   dialect: 'mysql',
+  dialectModule: mysql2,
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
